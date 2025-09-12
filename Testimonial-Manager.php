9daeaf2cfc31a723e4698006bfb934b30a178d14
Sajid Ashraf <sajidashraf.dev@gmail.com>
2025-09-13 00:23:12 +0500

    first commit

--- Testimonial-Manager.php
+++ Testimonial-Manager.php
@@ -11,21 +11,22 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
-
 function testimonial_manager_custom_post_type() {
-	register_post_type('testimonial',
-		array(
-			'labels'      => array(
-				'name'          => __('Testimonials', 'testimonial-manager'),
-				'singular_name' => __('Testimonial', 'testimonial-manager'),
-			),
-				'public'      => true,
-				'has_archive' => true,
-		)
-	);
+    register_post_type('testimonial',
+        array(
+            'labels'      => array(
+                'name'          => __('Testimonials', 'testimonial-manager'),
+                'singular_name' => __('Testimonial', 'testimonial-manager'),
+                'add_new_item'  => __('Add New Testimonial', 'testimonial-manager'),
+                'edit_item'     => __('Edit Testimonial', 'testimonial-manager'),
+                'new_item'      => __('New Testimonial', 'testimonial-manager'),
+                'view_item'     => __('View Testimonial', 'testimonial-manager'),
+            ),
+            'public'      => true,
+            'has_archive' => true,
+            'menu_position' => 5,
+            'menu_icon'   => 'dashicons-testimonial',
+        )
+    );
 }
 add_action('init', 'testimonial_manager_custom_post_type');
-
-
-
-?>
